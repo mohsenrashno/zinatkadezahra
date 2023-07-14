8f1e8b24ef58e875333a3066ee9851dec2302994
hozor <montazer12@chmail.ir>
2023-07-14 14:35:41 +0330

    configure breeze and make product page

---
+++ app/Models/Order.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Product;
+class Order extends Model
+{
+    use HasFactory;
+	
+		protected $fillable = [
+			'user_id',
+			'orderdate',
+			'canceleddate',
+			'paiddate',
+			'preparingdate',
+			'finisheddate',
+			'posteddate',
+			'deliverydate',
+			'paidstatus',
+			'postagefee',
+			'discount',
+			'price'
+	];
+	
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+	
+	public function products()
+	{
+		return $this->belongsToMany(Product::class);
+		
+	}
+}
---
+++ database/migrations/2023_06_26_130742_create_order_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+			$table->unsignedBigInteger('order_id');
+			$table->unsignedBigInteger('product_id');
+			$table->unsignedBigInteger('user_id');
+			$table->foreign('order_id')->references('id')->on('orders');
+			$table->foreign('product_id')->references('id')->on('products');
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
